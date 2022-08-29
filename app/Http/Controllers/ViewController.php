b2121e3ed3dbcbe1c72d3116b6a712a61a1e61a7
kylegoa <kyle.paulson92@gmail.com>
2022-08-28 20:35:07 -0600

    updated view controller

---
+++ app/Http/Controllers/ViewController.php
@@ -0,0 +1,31 @@
+<?php namespace App\Http\Controllers;
+
+use Auth;
+use Redirect;
+
+
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Response;
+
+
+use Artisan;
+
+class ViewController extends Controller {
+
+    /**purpose
+     *   run migrations
+     * args
+     *   (none)
+     * returns 
+     *   result
+     */
+	public function doMigrate(Request $request)
+	{
+		set_time_limit(60);
+		Artisan::call("migrate", ['--force' => 'default']);
+		$response = new Response;
+		return $response->jsonSuccess();
+	}
+
+}
