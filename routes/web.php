18ac27551e271adcbb01242213be0c9f872afbd1
kylegoa <kyle.paulson92@gmail.com>
2022-08-28 20:33:00 -0600

    added healthyr api is running

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
 // migrations
 Route::get('/migrate', 'ViewController@doMigrate');
 Route::post('/migrate', 'ViewController@doMigrate');
