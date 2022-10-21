0bb665f070680752f1b23ae24c29df254d95dd5d
kylegoa <kyle.paulson92@gmail.com>
2022-10-21 17:50:18 -0600

    updated custom

---
+++ app/Console/Commands/Custom.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use App\Models\Mysql;
+
+class Custom extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'custom';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Custom script';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        echo 'Custom';
+
+    }
+
+}
