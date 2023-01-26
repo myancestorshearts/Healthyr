0013264de094d1e7efeeadc69d53c9b5549b1425
ColeLucas2020 <cole.lucas@eliteworks.com>
2023-01-25 17:59:56 -0700

    need to pull

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,3 +4,4 @@ let mix = require('laravel-mix');
 
 mix.js(['resources/js/views/admin/index'], 'public/global/assets/js/views/admin.min.js');
 mix.js(['resources/js/views/authentication/index'], 'public/global/assets/js/views/authentication.min.js');
+mix.js(['resources/js/views/portal/index'], 'public/global/assets/js/views/portal.min.js');
